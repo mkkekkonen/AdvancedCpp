6e77c55e2b7fbc89156c83b4ad9583d0a4c94569
mkkekkonen2 <mkkekkonen@protonmail.com>
2020-05-09 23:55:31 +0300

    L2BE3 - constexpr

--- Lesson2B/Exercise03/Exercise3.cpp
+++ Lesson2B/Exercise03/Exercise3.cpp
@@ -10,44 +10,52 @@
 #include <iomanip>
 #include <string>
 #include <limits>
+#include <type_traits>
 
 template<typename T> std::string stringify(const T& x)
 {
     std::ostringstream out;
+    if constexpr (std::is_same_v<T, bool>) {
+    	out << std::boolalpha;
+	}
+    else if constexpr (std::is_floating_point_v<T>) {
+    	const int sigdigits = std::numeric_limits<T>::digits10;
+    	out << std::setprecision(sigdigits);
+    }
     out << x;
     return out.str();
 }
 
-template<> std::string stringify<bool>(const bool& x)
-{
-    std::ostringstream out;
-    out << std::boolalpha << x;
-    return out.str();
-}
-
-template<> std::string stringify<double>(const double& x)
-{
-    const int sigdigits = std::numeric_limits<double>::digits10;
-    std::ostringstream out;
-    out << std::setprecision(sigdigits) << x;
-    return out.str();
-}
-
-template<> std::string stringify<float>(const float& x)
-{
-    const int sigdigits = std::numeric_limits<float>::digits10;
-    std::ostringstream out;
-    out << std::setprecision(sigdigits) << x;
-    return out.str();
-}
-
-template<> std::string stringify<long double>(const long double& x)
-{
-    const int sigdigits = std::numeric_limits<long double>::digits10;
-    std::ostringstream out;
-    out << std::setprecision(sigdigits) << x;
-    return out.str();
-}
+//template<> std::string stringify<bool>(const bool& x)
+//{
+//    std::ostringstream out;
+//    out << std::boolalpha << x;
+//    return out.str();
+//}
+//
+//template<> std::string stringify<double>(const double& x)
+//{
+//    const int sigdigits = std::numeric_limits<double>::digits10;
+//    std::ostringstream out;
+//    out << std::setprecision(sigdigits) << x;
+//    return out.str();
+//}
+//
+//template<> std::string stringify<float>(const float& x)
+//{
+//    const int sigdigits = std::numeric_limits<float>::digits10;
+//    std::ostringstream out;
+//    out << std::setprecision(sigdigits) << x;
+//    return out.str();
+//}
+//
+//template<> std::string stringify<long double>(const long double& x)
+//{
+//    const int sigdigits = std::numeric_limits<long double>::digits10;
+//    std::ostringstream out;
+//    out << std::setprecision(sigdigits) << x;
+//    return out.str();
+//}
 
 
 int main(int argc, char**argv)
